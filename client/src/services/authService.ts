4b973eb513a9ccec3b6de4cb09479d6d75e1d7ab
Nick A <nicolas.a.asanov@gmail.com>
2025-08-29 15:16:59 -0400

    updated auth service

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,22 +1,23 @@
 import { authClient } from "@/lib/authClient";
 import { signIn, signOut } from "next-auth/react";
-import { InputUser } from "@/types/user";
-import { User } from "next-auth";
+import { AccountWithTokens, CreateAccountInput } from "@/types/dto/accountDto";
+
+const withBase = (path: string) => `/api/auth${path}`;
 
 /**
  * Authenticates a user based on email and password, adding the user to the session.
- * @param email - The email of the user.
- * @param password - The password of the user.
+ * @param accountData - The account data of the user, including email and password.
  * @param callbackUrl - The URL to redirect to after successful login (default `null`). If null, no redirection will occur.
  * @throws An `AxiosError` if there is a login conflict. Generic error if the NextAuth request fails.
  */
 async function login(
-  email: string,
-  password: string,
+  accountData: CreateAccountInput,
   callbackUrl: string | null = null
 ): Promise<void> {
+  const { email, password } = accountData;
+
   // Add the refresh token to the client
-  await authClient.post<User>("/auth/login", {
+  await authClient.post<AccountWithTokens>(withBase("/login"), {
     email,
     password,
   });
@@ -38,20 +39,20 @@ async function login(
 
 /**
  * Signs up a new user with the provided user data, adding the user to the session.
- * @param userData - The data of the user to sign up.
+ * @param accountData - The data of the user to sign up.
  * @param callbackUrl - The URL to redirect to after successful signup. If null, no redirection will occur. Defaults to "/".
  * @throws An `AxiosError` if there is a login conflict. Generic error if the NextAuth request fails.
  */
 async function signup(
-  userData: InputUser,
+  accountData: CreateAccountInput,
   callbackUrl: string | null = "/"
 ): Promise<void> {
-  await authClient.post<User>("/api/auth/signup", userData);
-  
+  await authClient.post<AccountWithTokens>(withBase("/signup"), accountData);
+
   const redirect = callbackUrl !== null;
   const res = await signIn("credentials", {
-    email: userData.email,
-    password: userData.password,
+    email: accountData.email,
+    password: accountData.password,
     callbackUrl: callbackUrl ?? undefined,
     redirect,
   });
@@ -68,7 +69,7 @@ async function signup(
  * @param callbackUrl - The URL to redirect to after logout. If null, no redirection will occur. Defaults to "/".
  */
 async function logout(callbackUrl: string | null = "/"): Promise<void> {
-  await authClient.post("/auth/logout");
+  await authClient.post(withBase("/logout"));
   await signOut({
     callbackUrl: callbackUrl ?? undefined,
     redirect: callbackUrl !== null,
